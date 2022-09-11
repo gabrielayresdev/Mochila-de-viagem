f4618142a6243d7ebe0824b6b04cd03d0d84435b
Gabriel Ayres <Gabriel.ar.fort@gmail.com>
2022-09-11 18:08:05 -0300

    Funcionalidade de apagar item

--- js/main.js
+++ js/main.js
@@ -24,9 +24,9 @@ submit.addEventListener("click", function (event) {
 
         atualizaElemento(itemAtual)
 
-        itens[existe.id] = itemAtual
+        itens[itens.findIndex(elemento => elemento.id === itemAtual.id)] = itemAtual
     } else {
-        itemAtual.id = itens.length
+        itemAtual.id = itens[itens.length - 1] ? (itens[itens.length - 1].id + 1) : 0
 
         criaElemento(itemAtual)
 
@@ -50,9 +50,32 @@ function criaElemento(item) {
     li.appendChild(strong)
     li.innerHTML += item.nome
 
+    li.appendChild(botaoDeleta(item.id))
+
     lista.appendChild(li)
 }
 
 function atualizaElemento(item) {
     document.querySelector(`[data-id="${item.id}"]`).innerHTML = item.quantidade
 }
+
+function botaoDeleta(id) {
+    const botao = document.createElement("button")
+    botao.innerHTML = "X"
+    botao.classList.add("btn")
+
+    botao.addEventListener("click", function () {
+        deletaElemento(this.parentNode, id)
+    })
+
+    return botao
+}
+
+function deletaElemento(tag, id) {
+    tag.remove()
+
+    itens.splice(id, 1)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
+}
+
